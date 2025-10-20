ee63b8e5602c98637aad0e3a16e0e5122f960e6c
Jason Merefield <jmerefield@deputy.com>
2025-10-20 11:51:34 +1100

    Adding S3 Bucket resource
    testing cloud integration

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -4,3 +4,17 @@ resource "random_password" "secret" {
   length  = 16
   special = true
 }
+
+resource "aws_s3_bucket" "my_bucket" {
+      bucket = "my-unique-terraform-s3-bucket-name-spaccelift"
+      acl    = "private"
+
+      tags = {
+        Environment = "Dev"
+        Project     = "MyApplication"
+      }
+
+      versioning {
+        enabled = true
+      }
+}
